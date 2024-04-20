dd077c76e86b57101988650142b9f0548f0e0016
Santiago <santiago991007@gmail.com>
2024-04-20 20:26:03 -0300

    Punto 3 terminado

--- tp3_3.c
+++ tp3_3.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CANT_NOMBRES 5
+#define LONG_NOMBRE 30
+
 char *listadoNombres[5];
 
 void main(){
+    
+    //Ingreso de nombres
     printf("Escribe 5 nombres: ");
-
-    for (int i=0; i<5; i++){
+    for (int i=0; i<CANT_NOMBRES; i++){
+        listadoNombres[i] = (char *)malloc(LONG_NOMBRE * sizeof(char));
         scanf("%s", listadoNombres[i]);
     }
 
+    //Mostrar nombres
     printf("Los nombres son:");
-    for (int i=0; i<5; i++){
-        printf("%s", listadoNombres[i]);
+    for (int i=0; i<CANT_NOMBRES; i++){
+        printf("\n%s", listadoNombres[i]);
+        free(listadoNombres[i]); //Liberar memoria
     }
 
-    //liberar memoria
-    for (int i=0; i<5; i++){
-        free(listadoNombres[i]);
-    }
-   
 }
